1ddd265e70706d010499aeb40a9e6f3910c9957e
kkoon9 <rndrnjs2003@gmail.com>
2021-04-17 08:58:31 +0900

    feat: get All Tasks
    - add model(interface)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
-import { TasksService } from './—no-spec/tasks/tasks.service';
-import { TasksService } from './—-no-spec/tasks/tasks.service';
-
+import { TasksService } from './tasks/tasks.service';
 @Module({
   imports: [TasksModule],
   providers: [TasksService],
