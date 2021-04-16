df783759fd82f80cc69323734cefa955be6b8be0
kkoon9 <rndrnjs2003@gmail.com>
2021-04-17 08:43:59 +0900

    feat: nest initial
    - controller
    - module
    - service
    - provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { TasksService } from './—no-spec/tasks/tasks.service';
+import { TasksService } from './—-no-spec/tasks/tasks.service';
 
 @Module({
   imports: [TasksModule],
+  providers: [TasksService],
 })
 export class AppModule {}
