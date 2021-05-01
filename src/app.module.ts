ab38ac2a6385898dfb21607e1fdf25ffec059c98
kkoon9 <rndrnjs2003@gmail.com>
2021-05-01 22:52:57 +0900

    feat: TypeORM으로 리팩토링

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
 import { TasksModule } from './tasks/tasks.module';
-import { TasksService } from './tasks/tasks.service';
 @Module({
-  imports: [TasksModule],
-  providers: [TasksService],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), TasksModule],
 })
 export class AppModule {}
