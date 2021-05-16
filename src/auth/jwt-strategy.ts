58e215282b25bb909547c02d97c099c93973e2b7
kkoon9 <rndrnjs2003@gmail.com>
2021-05-16 19:10:33 +0900

    feat: configuration 설정
    Signed-off-by: kkoon9 <rndrnjs2003@gmail.com>

--- src/auth/jwt-strategy.ts
+++ src/auth/jwt-strategy.ts
@@ -5,6 +5,10 @@ import { Strategy, ExtractJwt } from 'passport-jwt';
 import { JwtPayload } from "./jwt-payload.interface";
 import { User } from "./user.entity";
 import { UserRepository } from "./user.repository";
+import * as config from 'config';
+
+const jwtConfig = config.get('jwt');
+
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -14,7 +18,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 	) {
 		super({
 			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-			secretOrKey: 'topSecret51',
+			secretOrKey: process.env.JWT_SECRET || jwtConfig.secret,
 		});
 	}
 	
