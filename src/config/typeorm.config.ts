6851fee2095d88f95821f125535e329e98259b49
kkoon9 <rndrnjs2003@gmail.com>
2021-05-16 19:20:08 +0900

    feat: typeORM configuration 추가
    Signed-off-by: kkoon9 <rndrnjs2003@gmail.com>

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -11,6 +11,6 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   password: process.env.RDS_PASSWORD || dbConfig.password,
   database: process.env.RDS_DB_NAME || dbConfig.database,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
-  synchronize: process.env.TYPEORM_SYNC || dbConfig.synchronize,
+  synchronize: process.env.TYPEORM_SYNC  || dbConfig.synchronize,
   autoLoadEntities: true,
 };
