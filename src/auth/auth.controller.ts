a2661d534d12ef4f9ca5e8309b3c2c103211d002
kkoon9 <rndrnjs2003@gmail.com>
2021-05-16 11:47:21 +0900

    feat: jwt strategy & passport 기능 추가
    Signed-off-by: kkoon9 <rndrnjs2003@gmail.com>

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,10 @@
 import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+// import {Req, UseGuards} from '@netjs/common';
 import { AuthCredentialDto } from './dto/auth-credentials.dto';
 import { AuthService } from './auth.service';
+// import { AuthGuard } from '@nestjs/passport';
+// import { GetUser } from './get-user.decorator';
+import { User } from './user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -14,8 +18,13 @@ export class AuthController {
 	}
 
 	@Post('/signin')
-	signIn(@Body(ValidationPipe) authCredentialDto: AuthCredentialDto) {
+	signIn(@Body(ValidationPipe) authCredentialDto: AuthCredentialDto) : Promise<{accessToken : string}> {
 		return this.authService.signIn(authCredentialDto);
 	}
 
+	// @Post('/test')
+	// @UseGuards(AuthGuard()) 
+	// test(@GetUser() user: User ) {
+	// 	console.log(user); 
+	// }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialDto } from './dto/auth-credentials.dto';
+import { JwtPayload } from './jwt-payload.interface';
 import { UserRepository } from './user.repository';
 
 @Injectable()
@@ -8,14 +10,23 @@ export class AuthService {
     constructor(
         @InjectRepository(UserRepository)
         private UserRepository: UserRepository,
+        private JwtService: JwtService
     ){}
 
     signUp(authCredentialDto: AuthCredentialDto): Promise<void> {
         return this.UserRepository.signup(authCredentialDto);
     }
 
-    signIn(authCredentialDto: AuthCredentialDto) {
-        const result = this.UserRepository.validateUserPassword(authCredentialDto);
-        console.log(result);
+    async signIn(authCredentialDto: AuthCredentialDto) : Promise<{accessToken : string}> {
+        const username = await this.UserRepository.validateUserPassword(authCredentialDto);
+        
+        if(!username) {
+            throw new UnauthorizedException('Invalid credentials');
+        }
+
+        const payload: JwtPayload  = { username };
+        const accessToken = await this.JwtService.sign(payload);
+
+        return { accessToken };
     }
 }
