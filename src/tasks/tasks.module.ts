7fd5640e4127e5a381125cf1ae3ef368869c352e
kkoon9 <rndrnjs2003@gmail.com>
2021-05-16 12:09:13 +0900

    feat: task에 토큰 검사 기능 추가
    Signed-off-by: kkoon9 <rndrnjs2003@gmail.com>

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
 import { TaskReository } from './task.repository';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TaskReository])],
+  imports: [TypeOrmModule.forFeature([TaskReository]),AuthModule],
   controllers: [TasksController],
   providers: [TasksService],
 })
